9929782bbc0121c95ff22d23723e18bb0e028587
szywski27@gmail.com <LOki6262>
2021-09-16 18:50:31 +0200

    add models to admin

--- blog/api/admin.py
+++ blog/api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from api.models import BlogArticle, Tags
 # Register your models here.
+
+admin.site.register(BlogArticle)
+admin.site.register(Tags)
--- blog/api/models.py
+++ blog/api/models.py
@@ -3,19 +3,19 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
-class BlogArticle(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=100, blank=True, default='')
-    body = models.TextField(blank=True, default='')
-    owner = models.ForeignKey('auth.User', related_name='posts', on_delete=models.CASCADE)
-
-    class Meta:
-        ordering = ['created']
 
 class Tags(models.Model):
     tagName = models.CharField(max_length=50, blank=True, default='')
 
-class BlogTags(models.Model):
-    blogArticlePK = models.ForeignKey(BlogArticle, on_delete=models.CASCADE)
-    selectedTag = models.ForeignKey(Tags.tagName,on_delete=models.CASCADE)
+class BlogArticle(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    body = models.TextField(blank=True, default='')
+    owner = models.ForeignKey('auth.User', related_name='blogArticles', on_delete=models.CASCADE)
+    tagsSelected = models.ForeignKey(Tags,on_delete=models.CASCADE, blank=True, null=True)
+
+    class Meta:
+        ordering = ['created']
+
+
 
