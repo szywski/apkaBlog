fd33ddbc4c98cf722f2058e94ae0a14ffb9046b3
szywski27@gmail.com <LOki6262>
2021-09-16 19:00:43 +0200

    add blogArticles to serializer

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
+from api.models import BlogArticle
+
+
+
+
+class BlogArticleSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = BlogArticle
+        fields = ['id','title','body','owner','tagsSelected']
+
 class UserSerializer(serializers.ModelSerializer):
+    blogArticles = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     class Meta:
         model = User
-        fields = ['id','username','last_name']
+        fields = ['id', 'username', 'last_name']
