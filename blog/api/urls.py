fd0a3bb0be9d3f7261fa0bb404b7f4e80cf5d049
szywski27@gmail.com <LOki6262>
2021-09-20 21:44:18 +0200

    add blogarticles to view

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -5,6 +5,9 @@ from api import views
 urlpatterns = [
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('blogarticles/', views.BlogArticleList.as_view()),
+    path('blogarticles/<int:pk>', views.BlogArticleDetail.as_view()),
+
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
 from django.shortcuts import render
 from api import serializers
+from api.models import BlogArticle
+
 from django.contrib.auth.models import User
 
 class UserList(generics.ListCreateAPIView):
@@ -10,6 +12,17 @@ class UserList(generics.ListCreateAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
+
+class BlogArticleList(generics.ListCreateAPIView):
+    queryset = BlogArticle.objects.all()
+    serializer_class = serializers.BlogArticleSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class BlogArticleDetail(generics.RetrieveUpdateAPIView):
+    queryset = BlogArticle.objects.all()
+    serializer_class = serializers.BlogArticleSerializer
 
 
 # Create your views here.
